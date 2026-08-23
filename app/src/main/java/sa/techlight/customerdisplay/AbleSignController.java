ac1ea9bb22e8f1e5748921b0e8c3050fe16d3ad1
mazenmekawi <123121002+mazenmekawi@users.noreply.github.com>
2026-08-23 18:01:22 +0300

    Add AbleSign idle launcher

---
+++ app/src/main/java/sa/techlight/customerdisplay/AbleSignController.java
@@ -0,0 +1,19 @@
+package sa.techlight.customerdisplay;
+
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+
+public final class AbleSignController {
+    public static final String PACKAGE = "tv.ablesign.app";
+    private final Context context;
+    public AbleSignController(Context context){ this.context = context; }
+    public boolean openPlayer(){
+        PackageManager pm = context.getPackageManager();
+        Intent i = pm.getLaunchIntentForPackage(PACKAGE);
+        if(i == null) return false;
+        i.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+        context.startActivity(i);
+        return true;
+    }
+}
